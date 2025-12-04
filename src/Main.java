fedf4fd1fec471f4904f3996f587123c0de1d468
ayabenjamia <benjamia.a388@ucd.ac.ma>
2025-12-04 12:52:44 +0100

    Ajout du code de la classe Main et complétion des méthodes

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hotel Management System by aya");
-        System.out.println("Hotel Management System by aya benjamia s3 ");
-        // TODO: ajouter le code du projet ici
-    }
-}
